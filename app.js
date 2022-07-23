c03fb231ea2ecc8eb8816cdd4715f6efb2fe884e
Sebastian Marines <sebastian0marines@gmail.com>
2022-07-22 22:57:01 -0500

    Configure Mongo

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+const mongoose = require("mongoose");
+
 const express = require("express");
 const app = express();
 const port = 3000;
+
+mongoose.connect(process.env.MONGO_URI);
+mongoose.connection.on("connected", () => console.log("connected to mongo"));
 
 app.get("/", (req, res) => {
   res.send("Hello Poli-learning!");
