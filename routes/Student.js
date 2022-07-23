d82f81a27f7633f0c4cac8058eb4404b847f93f3
Sebastian Marines <sebastian0marines@gmail.com>
2022-07-23 02:40:13 -0500

    Check for unique users

--- routes/Student.js
+++ routes/Student.js
@@ -3,18 +3,36 @@ const router = express.Router();
 
 const Student = require("../models/Student");
 
-router.post("/", (req, res) => {
+router.post("/", (req, res, next) => {
   const { nick, name, age, rank, email, password } = req.body;
-  const student = new Student({
-    nick,
-    name,
-    age,
-    rank,
-    email,
+
+  console.log("Searching for student");
+  Student.findOne({ email: email }, function (err, student) {
+    if (err) {
+      return res.status(500).send(err);
+    } else if (student) {
+      return res.status(400).send("Student already exists");
+    } else {
+      Student.findOne({ nick: nick }, function (err, student) {
+        if (err) {
+          return res.status(500).send(err);
+        } else if (student) {
+          return res.status(400).send("Student already exists");
+        } else {
+          const student = new Student({
+            nick,
+            name,
+            age,
+            rank,
+            email,
+          });
+          student.setPassword(password);
+          student.save();
+          return res.send("Student created");
+        }
+      });
+    }
   });
-  student.setPassword(password);
-  student.save();
-  res.send("Student created");
 });
 
 module.exports = router;
